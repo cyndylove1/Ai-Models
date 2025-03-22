b0544e4feacd6efdd017509ae5682988251deb67
Nweke Cynthia Chinenye <nwekecynthia2015@gmail.com>
2025-03-22 14:26:13 -0700

    intial commit

--- src/pages/Hero/index.tsx
+++ src/pages/Hero/index.tsx
@@ -9,16 +9,16 @@ export default function Hero() {
         <div className="hero-section lg:h-screen h-[50rem]">
           <Navbar />
           <div className="lg:pt-0 ">
-            <h6 className="text-[#7191FF] pt-10 text-center text-[16px] leading-[38px] font-[700]">
+            <h6 className="text-[#7191FF] pt-10 font-[Plus Jakarta Sans] text-center text-[16px] leading-[38px] font-[700]">
               Leverage on Automation
             </h6>
             <h2 className="text-white text-center font-[figtree] text-[50px] md:text-[76px] leading-[82px] font-[600]">
-              AI Models for <br />  Business Solutions
+              AI Models for <br /> Business Solutions
             </h2>
-            <h5 className="text-white text-center px-2 md:text-[22px] text-[20px] leading-[38px] font-[500] py-4 lg:w-[70%] mx-auto">
+            <h5 className="text-white text-center font-[Plus Jakarta Sans] px-2 md:text-[22px] text-[20px] leading-[38px] font-[500] py-4 lg:w-[70%] mx-auto">
               Leverage the power of AI to automate, analyze, and optimize your
-              workflows. Our specialized models are designed to fit
-              different business needs.
+              workflows. Our specialized models are designed to fit different
+              business needs.
             </h5>
             <div className="flex justify-center">
               <Buttons
@@ -29,8 +29,8 @@ export default function Hero() {
             </div>
           </div>
         </div>
-            <CompanyLogo />
-            <Models />    
+        <CompanyLogo />
+        <Models />
       </>
     );
 }
